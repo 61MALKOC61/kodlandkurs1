f33423429f109dda8dd6c4f25288e5686f883b95
61MALKOC61 <159919344+61MALKOC61@users.noreply.github.com>
2024-02-14 21:30:03 +0300

    Update ders1.py

--- ders1.py
+++ ders1.py
@@ -1,1 +1,7 @@
-sözlük
+a=input("Merhaba hocam,nasılsınız?)
+if a = "iyi":
+  print("Buna çok sevindim.")
+elif a ="kötü":
+  print("Buna çok üzüldüm.")
+else:
+  print("Bu duyguyu küçük harfle yazın yada henüz bu duyguyu daha eklemedim.")
